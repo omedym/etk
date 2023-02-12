d05c93c27f6f9069463539a6781c295b2567ec41
David Vins <david.vins@gmail.com>
2023-02-12 15:00:30 -0500

    feat(core): Require TenantId when building message instances
    Refs: OD-5816

--- packages/core/src/message/base/MessageFactory.ts
+++ packages/core/src/message/base/MessageFactory.ts
@@ -14,19 +14,28 @@ export abstract class AbstractMessageFactory<
   abstract definition: IMessageDefinition;
 
   build(
+    tenantId: string,
     data: TData,
     metadata?: TMetadata,
   ): TMessage {
+    // When constructing the message we inject the provided tenantId across
+    // all multiple concerns, specifically the event attribute `tenantid`, and
+    // both a data payload and context payload attribute for `tenantId`.
     const message: TMessage = {
       type: this.definition.cloudEvent.type,
       now: DateTime.now().toISO(),
       id: createId(),
-      data,
+      data: {
+        ...data,
+        tenantId: tenantId,
+      },
       metadata: metadata ? metadata : DefaultMessageMetadata,
       specversion: '1.0',
-      tenantid: '',
+      tenantid: tenantId,
       source: '',
-      context: {},
+      context: {
+        tenantId: tenantId,
+      },
     } as unknown as TMessage;
 
     return message;
