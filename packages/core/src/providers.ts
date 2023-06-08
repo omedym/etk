a1137d453e084364db65b70a1de4fb53ea9790ea
David Vins <david.vins@gmail.com>
2023-06-08 19:52:14 -0400

    fix(core): Fix issue, TrackedJobEventQueue is not using Redis queue suffix when provided
    Refs: OD-6326

--- packages/core/src/providers.ts
+++ packages/core/src/providers.ts
@@ -1,4 +1,10 @@
+const {
+  REDIS_QUEUE_SUFFIX = '',
+} = process.env;
+
+export const QueueSuffix = REDIS_QUEUE_SUFFIX ? `-${REDIS_QUEUE_SUFFIX}` : undefined;
+
 export const Providers = {
   ILogger: 'ILOGGER',
-  TrackedJobEventQueue: '__trackedJobEvents',
+  TrackedJobEventQueue: '__TrackedJobEvents' + QueueSuffix,
 }
